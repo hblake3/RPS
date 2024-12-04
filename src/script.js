9dc26e2bb0b2134c840f21aa89c06c00b80aecc7
hblake3 <155304179+hblake3@users.noreply.github.com>
2024-12-04 14:38:10 -0500

    add selection class to computer choice

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 let numLives, numWins;
 let playerName = 'PLR';
 
-function setPlayerInitials(){
-    console.log("test");
-}
-
 const rules = {
     rock: {beats: 'scissors'},
     paper: {beats: 'rock'},
@@ -16,12 +12,17 @@ const compChoiceText_el = document.getElementById("compChoiceText");
 const winsText_el = document.getElementById("winsText");
 const livesText_el = document.getElementById("livesText");
 let playerButtons = []; //holds all player button objects
+let computerButtons = []; //holds all player button objects
 
 // create an array of all player buttons
 document.querySelectorAll('#gameButtons button').forEach(button => {
     playerButtons.push(button);
 });
 
+// create an array of all computer buttons
+document.querySelectorAll('#gameButtons-PC button').forEach(button => {
+    computerButtons.push(button);
+});
 
 // initialize the game
 document.addEventListener("DOMContentLoaded", () => {
@@ -47,22 +48,28 @@ function runGame(choice){
         playerButtons.forEach(button =>{
             button.classList.remove("rps-selected");
             button.classList.add("rps-button-inactive");
-        })
-
-        // document.querySelectorAll('#gameButtons button').forEach(button => {
-        //     button.classList.remove("rps-selected");
-        //     button.classList.add("rps-button-inactive");
-        // });
-
+        });
         choice.classList.remove("rps-button-inactive");
         choice.classList.add("rps-selected");
-
 
         // get computer choice
         const compChoices = Object.keys(rules);
         let compChoice = compChoices[Math.floor(Math.random() * compChoices.length)];
         compChoiceText_el.innerHTML = (`Computer chose ${compChoice}!`);
         
+        // reset the computer's buttons to default coloring
+        computerButtons.forEach(button => {
+            button.classList.remove("rps-selected-PC");
+            button.classList.add("rps-button-inactive");
+        });
+
+        // apply highlight to the computer's choice
+        computerButtons.forEach(button => {
+            if(button.value === compChoice){
+                button.classList.add("rps-selected-PC");
+                console.log(`${button.value} classes: ${button.classList}`);
+            }
+        });
         
         // run the game
         if(choice.value === compChoice){
